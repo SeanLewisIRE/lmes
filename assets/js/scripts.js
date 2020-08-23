3a16e7403638f80cafab216b3ef412945eb42f2d
SeanLewisIRE <slewis100@gmail.com>
2020-08-23 17:36:25 +0100

    Moved JS to seperate file. Contact form emailJS working. Added sweet alerts for contact form success/failure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,14 @@
 
-function sendEmail() {
-    
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        emailjs.sendForm('default_service', 'template_JoyOOxUq', this)
+        .then(function (response) {
+            swal("Message Sent Sucessfully", "Thank you for your message. We will get back to you as soon as possible", "success");
+            document.getElementById('contact-form').reset();
+        }, function (error) {
+                swal("Message Error", "Unfortunately your message couldn't be sent at this time. Please try again later or email Liam at liam@lmes.ie directly.", "error");
+        });
+    });
 }
+
